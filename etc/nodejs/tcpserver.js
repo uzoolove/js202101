325063f837d0ba72b9d131ab3897315fec6a6675
uzoolove@gmail.com <uzoolove@gmail.com>
2021-01-06 11:09:44 +0900

    .

--- etc/nodejs/tcpserver.js
+++ etc/nodejs/tcpserver.js
@@ -2,7 +2,10 @@ const net = require('net');
 
 net.createServer(function(socket){
   console.log(socket.remoteAddress, '접속');
-  
+  socket.on('data', function(data){
+    console.log('from client:', data.toString());
+    socket.write(data);
+  });
 }).listen(1234, function(){
   console.log('TCP 서버 구동.');
 });
