ac7d58d66d897153ffb3ff74e89de85ae3420b22
uzoolove@gmail.com <uzoolove@gmail.com>
2021-01-06 15:08:50 +0900

    .

--- js/mylib.js
+++ js/mylib.js
@@ -1,29 +1,31 @@
-var ajax = function(url, method, async, data, dataType, success){
-  method = method || 'get';
-  if(async === undefined){
-    async = true;
+// var ajax = function(url, method, async, data, dataType, success){
+var ajax = function(url, options){
+  options = options || {};
+  options.method = options.method || 'get';
+  if(options.async === undefined){
+    options.async = true;
   }
-  data = data || '';
-  dataType = dataType || 'text';
+  options.data = options.data || '';
+  options.dataType = options.dataType || 'text';
 
-  if(method.toLowerCase()=='get' && data){
-    url += '?' + data;
-    data = '';
+  if(options.method.toLowerCase()=='get' && options.data){
+    url += '?' + options.data;
+    options.data = '';
   }
   // 1. XMLHttpRequest 생성		
   var xhr = new XMLHttpRequest();
-  if(success){
+  if(options.success){
     xhr.onload = function(){
       // 4. 응답 데이터 처리
       var result = xhr.responseText;
-      if(dataType.toLowerCase()=='json'){
+      if(options.dataType.toLowerCase()=='json'){
         result = JSON.parse(result);
       }
-      success(result);
+      options.success(result);
     }
   }
   // 2. 요청준비(open())
-  xhr.open(method, url, async);
+  xhr.open(options.method, url, options.async);
   // 3. 요청(send())
-  xhr.send(data);
+  xhr.send(options.data);
 };
