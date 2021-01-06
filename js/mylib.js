188788133be01ce1234fcdb9ac34013d556a1021
uzoolove@gmail.com <uzoolove@gmail.com>
2021-01-06 14:35:38 +0900

    .

--- js/mylib.js
+++ js/mylib.js
@@ -1,17 +1,26 @@
 var ajax = function(url, method, async, data, dataType, success){
+  method = method || 'get';
+  if(async === undefined){
+    async = true;
+  }
+  data = data || '';
+  dataType = dataType || 'text';
+
   if(method.toLowerCase()=='get' && data){
     url += '?' + data;
     data = '';
   }
   // 1. XMLHttpRequest 생성		
   var xhr = new XMLHttpRequest();
-  xhr.onload = function(){
-    // 4. 응답 데이터 처리
-    var result = xhr.responseText;
-    if(dataType.toLowerCase()=='json'){
-      result = JSON.parse(result);
+  if(success){
+    xhr.onload = function(){
+      // 4. 응답 데이터 처리
+      var result = xhr.responseText;
+      if(dataType.toLowerCase()=='json'){
+        result = JSON.parse(result);
+      }
+      success(result);
     }
-    success(result);
   }
   // 2. 요청준비(open())
   xhr.open(method, url, async);
