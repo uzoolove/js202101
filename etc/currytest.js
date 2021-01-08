baf8ae9ac4c63db0adf5c968a4233667a8ec71f1
uzoolove@gmail.com <uzoolove@gmail.com>
2021-01-08 11:29:47 +0900

    ,

--- etc/currytest.js
+++ etc/currytest.js
@@ -1,8 +1,8 @@
-console.log(Math.max(1000, 500, 300, 400)); // 1000
-console.log(Math.max(1, 6, 7, 3, 6, 4, 2)); // 7
+console.log(Math.max(10, 1000, 500, 300, 400)); // 1000
+console.log(Math.max(10, 1, 6, 7, 3, 6, 4, 2)); // 7
 
-console.log(Math.min(1000, 500, 300, 400)); // 300
-console.log(Math.min(1, 6, 7, 3, 6, 4, 2)); // 1
+console.log(Math.min(100, 1000, 500, 300, 400)); // 300
+console.log(Math.min(100, 1, 6, 7, 3, 6, 4, 2)); // 1
 
 /*
 함수명: maxAbove10
@@ -12,7 +12,11 @@ console.log(Math.min(1, 6, 7, 3, 6, 4, 2)); // 1
 */
 var maxAbove10 = function(){
   // etc/smallest.js 참조
-  
+  var max = Math.max.apply(this, arguments);
+  if(max < 10){
+    max = 10;
+  }
+  return max;
 };
 
 /*
@@ -22,8 +26,18 @@ var maxAbove10 = function(){
 즉, 최대 100 이하의 결과가 나와야 한다.
 */
 var minUnder100 = function(){
-	
+  var fn = Math.min;
+  var preArgs = [100];
+  // [1000, 500, 300, 400]
+  var callArgs = Array.prototype.slice.call(arguments);
+  // [100, 1000, 500, 300, 400]
+  var args = preArgs.concat(callArgs);
+  // Math.min(100, 1000, 500, 300, 400)
+  return fn.apply(this, args);
 };
+
+var maxAbove10 = Math.max.mycurry(10);
+var minUnder100 = Math.min.mycurry(100);
 
 console.log(maxAbove10(1000, 500, 300, 400));// 1000
 console.log(maxAbove10(1, 6, 7, 3, 6, 4, 2));// 10
